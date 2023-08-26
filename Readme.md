f313c973a7cdaf9b438e994efa249b07cab06a21
coder-rancho <coder.namanvyas@gmail.com>
2023-08-26 15:27:53 +0530

    created session

--- Readme.md
+++ Readme.md
@@ -1,1 +1,6 @@
 Reference: [Blog on "Easy google auth with nodejs"](https://medium.com/@jackrobertscott/easy-google-auth-with-node-js-99ac40b97f4c)
+
+##Session
+Reference: [How Session work in express](https://medium.com/@alysachan830/cookie-and-session-ii-how-session-works-in-express-session-7e08d102deb8)
+
+Reference: [express-session npm](https://www.npmjs.com/package/express-session)
--- app.js
+++ app.js
@@ -2,13 +2,24 @@ const express = require('express')
 const { googleLoginUrl } = require('./utils/google-oauth-loginUrl')
 const { getAccessTokenFromCode } = require('./utils/google-oauth-accessToken')
 const { getGoogleUserEmail } = require('./utils/google-oauth-userEmail')
+const session = require('express-session')
+require('dotenv').config()
 
 const app = express()
 
-app.get('/', (req, res) => res.send('hello'))
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.get('/', (req, res) => {
+    if (!req.session.authorized) res.redirect('/auth/google/login')
+    res.send(`Hello ${req.session.userId}`)
+})
 
 app.get('/auth/google/login', (req, res) => {
-    res.send(googleLoginUrl)
+    res.redirect(googleLoginUrl)
 })
 
 app.get('/auth/google/callback', async (req, res) => {
@@ -21,6 +32,9 @@ app.get('/auth/google/callback', async (req, res) => {
     const code = req.query.code
     const token = await getAccessTokenFromCode(code)
     const email = await getGoogleUserEmail(token)
+
+    req.session.authorized = true
+    req.session.userId = /* User.findOne({email}) || */ email
 
     res.send(email)
 })
